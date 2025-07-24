86b7a5cc61ddc45ed2d078b93308ba615b633bba
ZAIN <161834899+ItsZaYn007@users.noreply.github.com>
2025-07-24 23:12:04 +0500

    Delete Static directory

--- Static/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.getElementById("loading").style.display = "none";
-        document.getElementById("prediction-box").classList.remove("hidden");
-    }, 1500);
-});
